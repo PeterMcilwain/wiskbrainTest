ab5ae1fd4525e96ec2ef1fb7994bbf074bee2e0a
PeterMcilwain <88867509+PeterMcilwain@users.noreply.github.com>
2021-08-13 14:28:51 +1000

    Add files via upload

---
+++ wiskbrainJS.js
@@ -0,0 +1,35 @@
+var images = [];
+var img = 0;
+var d, x, y;
+
+function preload() {
+  for (var i = 0; i < 18; i++) {
+    images[i] = loadImage("assets/face" + i + ".png");
+  }
+}
+
+function setup() {
+  if (windowWidth > windowHeight) {
+    d = windowHeight;
+  } else {
+    d = windowWidth;
+  }
+
+  createCanvas(windowWidth, windowHeight);
+  x = (windowWidth - d) / 2;
+  y = (windowHeight - d) / 2;
+}
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(255);
+  getSector();
+  image(images[img], x, y, d, d);
+}
+
+function getSector() {
+  var a = atan2(mouseY-height/2, mouseX-width/2);
+  img = (int(map(a, PI, -PI, 0, 17)) + 1) % 18;
+}
