094c708bc0df29df23aee247c4fb6d7a059cbd81
PeterMcilwain <88867509+PeterMcilwain@users.noreply.github.com>
2021-08-14 11:32:03 +1000

    Add files via upload

--- wiskbrainJS.js
+++ wiskbrainJS.js
@@ -9,27 +9,38 @@ function preload() {
 }
 
 function setup() {
-  if (windowWidth > windowHeight) {
-    d = windowHeight;
-  } else {
-    d = windowWidth;
-  }
-
   createCanvas(windowWidth, windowHeight);
-  x = (windowWidth - d) / 2;
-  y = (windowHeight - d) / 2;
+  fullscreen(true);
+  getDrawPosition();
 }
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
-
 function draw() {
   background(255);
   getSector();
   image(images[img], x, y, d, d);
 }
-
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  getDrawPosition();
+}
+function getDrawPosition() {
+  if (windowWidth > windowHeight) {
+    d = windowHeight;
+  } else {
+    d = windowWidth;
+  }
+  x = (windowWidth - d) / 2;
+  y = (windowHeight - d) / 2;
+}
 function getSector() {
   var a = atan2(mouseY-height/2, mouseX-width/2);
   img = (int(map(a, PI, -PI, 0, 17)) + 1) % 18;
 }
+function mousePressed() {
+  if (mouseX > 0 && mouseX < 100 && mouseY > 0 && mouseY < 100) {
+    let fs = fullscreen();
+    fullscreen(!fs);
+  }
+}
+document.ontouchmove = function(event) {
+    event.preventDefault();
+};
